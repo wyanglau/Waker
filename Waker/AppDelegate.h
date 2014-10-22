2a0760591bcda11d74a6b2fb40361c297ec653c5
Liuwyang <liuwyang@user230-226.wireless.utoronto.ca>
2014-10-21 22:26:47 -0400

    Initial Commit

---
+++ Waker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Waker
+//
+//  Created by Liuwyang on 2014-10-21.
+//  Copyright (c) 2014 ECE1780. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Waker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Waker
+//
+//  Created by Liuwyang on 2014-10-21.
+//  Copyright (c) 2014 ECE1780. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
